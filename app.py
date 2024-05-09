0e4245955e208b2b6c5aa8f0d8a35758bdb5965d
Deathstroke751 <mightyroyalgiant@gmail.com>
2024-05-10 01:35:58 +0530

    args

--- app.py
+++ app.py
@@ -1,22 +1,29 @@
 from flask import Flask, jsonify, request
 import requests
-from flask_cors import CORS  # Import CORS from Flask-CORS
+# from flask_cors import CORS  # Import CORS from Flask-CORS
 
 app = Flask(__name__)
-CORS(app)  # Enable CORS for all routes
+# CORS(app)  # Enable CORS for all routes
 
 
 @app.route('/')
 def index():
-    return jsonify(message="Flask Relay Program is working!"), 200
+    return jsonify({"version": "1", "message": "Flask Relay Program is working!"}), 200
 
 
 @app.route('/relay', methods=['GET', 'POST'])
 def relay():
+    _args = request.args.to_dict()
     link = request.args.get(
         'link') if request.method == 'GET' else request.form.get('link')
     if not link:
         return jsonify(error="Link parameter is missing"), 400
+    # Iterate through the dictionary and construct the URL string
+    for key, value in _args.items():
+        if key == 'link':
+            link = value
+        else:
+            link += f"&{key}={value}"
 
     try:
         if request.method == 'GET':
